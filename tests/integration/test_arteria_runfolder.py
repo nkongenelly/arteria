4f7cdab6b948184e05a0e43d0128e07929e5f437
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-06-27 12:55:00 +0200

    Lint tests

--- tests/integration/test_arteria_runfolder.py
+++ tests/integration/test_arteria_runfolder.py
@@ -3,16 +3,14 @@ import importlib.metadata
 
 from arteria.services.arteria_runfolder import get_app
 
+
 class ArteriaRunfolderTestCase(AioHTTPTestCase):
     async def get_application(self):
         return get_app()
 
     async def test_version(self):
         async with self.client.request("GET", "/version") as resp:
-            self.assertEqual(resp.status, 200)
+            assert resp.status == 200
             content = await resp.json()
 
-        self.assertEqual(
-            content,
-            {"version": importlib.metadata.version("arteria") }
-        )
+        assert content == {"version": importlib.metadata.version("arteria")}
