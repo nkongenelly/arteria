566189ca8064d04a843da13ce3790965a3d561f9
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-06-28 10:12:42 +0200

    Add integration test POST /runfolders/path/
    Co-authored-by: nkongenelly <nkongenelly94@gmail.com>

--- tests/integration/test_arteria_runfolder.py
+++ tests/integration/test_arteria_runfolder.py
@@ -1,12 +1,30 @@
 from aiohttp.test_utils import AioHTTPTestCase
 import importlib.metadata
+from pathlib import Path
+
+import tempfile
 
 from arteria.services.arteria_runfolder import get_app
 
 
 class ArteriaRunfolderTestCase(AioHTTPTestCase):
     async def get_application(self):
-        return get_app()
+        monitored_dir = tempfile.TemporaryDirectory()
+        # TODO clean up this temporary file after the tests are done
+
+        runfolder = (
+                Path(monitored_dir.name)
+                / "200624_A00834_0183_BHMTFYDRXX"
+        )
+        runfolder.mkdir()
+
+        self.config = {
+            "monitored_directories": [
+                monitored_dir.name
+            ]
+        }
+
+        return get_app(self.config)
 
     async def test_version(self):
         async with self.client.request("GET", "/version") as resp:
@@ -14,3 +32,18 @@ class ArteriaRunfolderTestCase(AioHTTPTestCase):
             content = await resp.json()
 
         assert content == {"version": importlib.metadata.version("arteria")}
+
+    async def test_post_runfolders_path(self):
+        async with self.client.request(
+                "POST",
+                "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
+                data={"state": "STARTED"}) as resp:
+            assert resp.status == 200
+
+            state = (
+                Path(self.config["monitored_directories"][0])
+                / ".arteria/state"
+            )
+
+            with open(state) as state_file:
+                assert state_file.read() == "started"
