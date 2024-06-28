274600cf40dd53390cc36dce43174ef08b072a56
nkongenelly <nkongenelly94@gmail.com>
2024-06-28 11:40:14 +0300

    Added more test for /runfolders/path
    Co-authored-by: Adrien Coulier <adrien.coulier@medsci.uu.se>

--- tests/integration/test_arteria_runfolder.py
+++ tests/integration/test_arteria_runfolder.py
@@ -10,7 +10,7 @@ from arteria.services.arteria_runfolder import get_app
 class ArteriaRunfolderTestCase(AioHTTPTestCase):
     async def get_application(self):
         monitored_dir = tempfile.TemporaryDirectory()
-        # TODO clean up this temporary file after the tests are done
+        TODO clean up this temporary file after the tests are done
 
         runfolder = (
                 Path(monitored_dir.name)
@@ -47,3 +47,22 @@ class ArteriaRunfolderTestCase(AioHTTPTestCase):
 
             with open(state) as state_file:
                 assert state_file.read() == "started"
+
+    async def test_post_runfolders_path_invalid_state(self):
+        async with self.client.request(
+                "POST",
+                "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
+                data={"state": "INVALID"}) as resp:
+
+            assert resp.status == 400
+            assert resp.text == "The state 'INVALID' is not valid"
+
+    async def test_post_runfolders_path_missing_runfolder(self):
+        async with self.client.request(
+                "POST",
+                "/runfolders/path/200624_A00834_0183_FAKE_RUNFOLDER",
+                data={"state": "STARTED"}) as resp:
+
+            assert resp.status == 404
+            assert resp.text == "Runfolder '200624_A00834_0183_FAKE_RUNFOLDER' does not exist"
+            
