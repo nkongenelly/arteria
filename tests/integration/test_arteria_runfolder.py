5f3e8e0573eefe30f7adf3ddb93e4aedfb2b051a
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-06-28 13:00:06 +0200

    Switch to pytest fixtures

--- tests/integration/test_arteria_runfolder.py
+++ tests/integration/test_arteria_runfolder.py
@@ -1,68 +1,65 @@
-from aiohttp.test_utils import AioHTTPTestCase
 import importlib.metadata
 from pathlib import Path
-
 import tempfile
+
+import pytest
 
 from arteria.services.arteria_runfolder import get_app
 
 
-class ArteriaRunfolderTestCase(AioHTTPTestCase):
-    async def get_application(self):
-        monitored_dir = tempfile.TemporaryDirectory()
-        TODO clean up this temporary file after the tests are done
-
-        runfolder = (
-                Path(monitored_dir.name)
-                / "200624_A00834_0183_BHMTFYDRXX"
-        )
+@pytest.fixture()
+def config():
+    with tempfile.TemporaryDirectory() as monitored_dir:
+        runfolder = Path(monitored_dir) / "200624_A00834_0183_BHMTFYDRXX"
         runfolder.mkdir()
 
-        self.config = {
-            "monitored_directories": [
-                monitored_dir.name
-            ]
+        yield {
+            "monitored_directories": [monitored_dir],
         }
 
-        return get_app(self.config)
 
-    async def test_version(self):
-        async with self.client.request("GET", "/version") as resp:
-            assert resp.status == 200
-            content = await resp.json()
+@pytest.fixture()
+async def client(config, aiohttp_client):
+    app = get_app(config)
+    return await aiohttp_client(app)
 
-        assert content == {"version": importlib.metadata.version("arteria")}
 
-    async def test_post_runfolders_path(self):
-        async with self.client.request(
-                "POST",
-                "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
-                data={"state": "STARTED"}) as resp:
-            assert resp.status == 200
+async def test_version(client):
+    async with client.request("GET", "/version") as resp:
+        assert resp.status == 200
+        content = await resp.json()
 
-            state = (
-                Path(self.config["monitored_directories"][0])
-                / ".arteria/state"
-            )
+    assert content == {"version": importlib.metadata.version("arteria")}
 
-            with open(state) as state_file:
-                assert state_file.read() == "started"
 
-    async def test_post_runfolders_path_invalid_state(self):
-        async with self.client.request(
-                "POST",
-                "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
-                data={"state": "INVALID"}) as resp:
+async def test_post_runfolders_path(client, config):
+    async with client.request(
+            "POST",
+            "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
+            data={"state": "STARTED"}) as resp:
+        assert resp.status == 200
 
-            assert resp.status == 400
-            assert resp.text == "The state 'INVALID' is not valid"
+        state = Path(config["monitored_directories"][0]) / ".arteria/state"
 
-    async def test_post_runfolders_path_missing_runfolder(self):
-        async with self.client.request(
-                "POST",
-                "/runfolders/path/200624_A00834_0183_FAKE_RUNFOLDER",
-                data={"state": "STARTED"}) as resp:
+        with open(state) as state_file:
+            assert state_file.read() == "started"
 
-            assert resp.status == 404
-            assert resp.text == "Runfolder '200624_A00834_0183_FAKE_RUNFOLDER' does not exist"
-            
+
+async def test_post_runfolders_path_invalid_state(client):
+    async with client.request(
+            "POST",
+            "/runfolders/path/200624_A00834_0183_BHMTFYDRXX",
+            data={"state": "INVALID"}) as resp:
+
+        assert resp.status == 400
+        assert resp.text == "The state 'INVALID' is not valid"
+
+
+async def test_post_runfolders_path_missing_runfolder(client):
+    async with client.request(
+            "POST",
+            "/runfolders/path/200624_A00834_0183_FAKE_RUNFOLDER",
+            data={"state": "STARTED"}) as resp:
+
+        assert resp.status == 404
+        assert resp.text == "Runfolder '200624_A00834_0183_FAKE_RUNFOLDER' does not exist"
