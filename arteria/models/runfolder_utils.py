b07431cbec82d7c5ee48606ec7032342eb4e9918
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-07-03 13:48:19 +0200

    Implement metadata for Runfolder

--- arteria/models/runfolder_utils.py
+++ arteria/models/runfolder_utils.py
@@ -1,6 +1,12 @@
 from pathlib import Path
+import logging
+
+import xmltodict
 
 from arteria.models.state import State
+
+
+log = logging.getLogger(__name__)
 
 
 def list_runfolders(path, filter_key=lambda r: True):
@@ -19,6 +25,20 @@ class Runfolder:
         if not self._state_file.exists():
             self._state_file.write_text("ready")
 
+        try:
+            run_parameter_file = next(
+                path
+                for path in [
+                    self.path / "RunParameters.xml",
+                    self.path / "runParameters.xml",
+                ]
+                if path.exists()
+            )
+            self.run_parameters = xmltodict.parse(run_parameter_file.read_text())["RunParameters"]
+        except StopIteration:
+            self.run_parameters = {}
+            log.warning(f"File [Rr]unParameters.xml not found in runfolder {path}")
+
     @property
     def state(self):
         return State(self._state_file.read_text().strip())
@@ -28,10 +48,35 @@ class Runfolder:
         assert new_state in State
         self._state_file.write_text(new_state.value)
 
-
     @property
     def metadata(self):
-        pass
+        if not self.run_parameters:
+            log.warning(f"No metadata found for runfolder {self.path}")
+
+        metadata = {}
+
+        try:
+            metadata["reagent_kit_barcode"] = \
+                self.run_parameters["ReagentKitBarcode"]
+        except KeyError:
+            log.debug("Reagent kit barcode not found")
+
+        try:
+            metadata["library_tube_barcode"] = \
+                self.run_parameters["RfidsInfo"]["LibraryTubeSerialBarcode"]
+        except KeyError:
+            try:
+                metadata["library_tube_barcode"] = \
+                    next(
+                        consumable["SerialNumber"]
+                        for consumable in self.run_parameters["ConsumableInfo"]["ConsumableInfo"]
+                        if consumable["Type"] == "SampleTube"
+                    )
+            except (KeyError, StopIteration):
+                log.debug("Library tube barcode not found")
+
+        return metadata
+
 
 class Instrument:
     def __init__(self, run_params_file):
--- arteria/models/state.py
+++ arteria/models/state.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class State(Enum):
     NONE = "none"
     READY = "ready"
