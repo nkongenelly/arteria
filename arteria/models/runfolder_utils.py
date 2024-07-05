9d0e6943f110e4f366336ecc03f4145db655e58e
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-07-05 09:43:18 +0200

    Document runfolder_utils

--- arteria/models/runfolder_utils.py
+++ arteria/models/runfolder_utils.py
@@ -28,7 +28,6 @@ def list_runfolders(monitored_directories, filter_key=lambda r: True):
                 if e == f"File [Rr]unParameters.xml not found in runfolder {subdir}":
                     continue
 
-    return runfolders
 
 class Runfolder():
     """
