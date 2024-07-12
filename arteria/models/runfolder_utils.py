109c4e9b5447a098491605e294f837d3a459bb71
Adrien Coulier <adrien.coulier@medsci.uu.se>
2024-07-12 18:03:38 +0200

    Add default variables to Config

--- arteria/models/runfolder_utils.py
+++ arteria/models/runfolder_utils.py
@@ -10,6 +10,10 @@ from arteria.models.config import Config
 
 log = logging.getLogger(__name__)
 
+DEFAULT_CONFIG = {
+    "completed_marker_grace_minutes": 0,
+}
+
 
 def list_runfolders(monitored_directories, filter_key=lambda r: True):
     """
@@ -35,6 +39,7 @@ class Runfolder():
     A class to manipulate runfolders on disk
     """
     def __init__(self, path):
+        self.config = Config(DEFAULT_CONFIG)
         self.path = Path(path)
         assert self.path.is_dir()
         try:
@@ -56,7 +61,7 @@ class Runfolder():
             marker_file.exists()
             and (
                 time.time() - os.path.getmtime(marker_file)
-                > Config().get("completed_marker_grace_minutes", 0) * 60
+                > self.config["completed_marker_grace_minutes"] * 60
             )
         )
 
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,7 +2,7 @@ import pytest
 import tempfile
 import yaml
 
-from arteria.models.config import Config, DEFAULT_CONFIG_DICT
+from arteria.models.config import Config
 
 
 @pytest.fixture()
@@ -24,6 +24,7 @@ def config(config_dict):
 
 
 def test_config_from_yaml(config_dict):
+    # TODO test `exist_ok`
     with tempfile.NamedTemporaryFile(mode="r+") as config_file:
         config_file.write(yaml.dump(config_dict))
         config_file.seek(0)
@@ -63,15 +64,41 @@ def test_config_immutable(config, config_dict):
 
 
 def test_config_new(config, config_dict):
+    # TODO add comments to explain expected behavior
     assert Config().to_dict() == config_dict
 
     assert Config() == Config()
 
     new_config = {"abc": "123"}
-    Config.new(new_config)
+
+    with pytest.raises(AssertionError):
+        Config.new(new_config)
+
+    Config.new(new_config, exist_ok=True)
     assert config.to_dict() == new_config
     assert Config().to_dict() == new_config
 
 
-def test_default_config():
-    assert Config().to_dict() == DEFAULT_CONFIG_DICT
+def test_default_config_from_scratch():
+    # TODO add comments to explain expected behavior
+    defaults = {"default_variable": 1}
+    config = Config(defaults)
+    assert config.to_dict() == defaults
+    assert config["default_variable"] == defaults["default_variable"]
+
+    defaults["default_variable"] = 0
+    assert config["default_variable"] != defaults["default_variable"]
+
+
+def test_default_config_from_existing_config(config, config_dict):
+    # TODO add comments to explain expected behavior
+    defaults = {"default_variable": 1, "port": 0}
+    config = Config(defaults)
+
+    expected_dict = {k: v for k, v in defaults.items()}
+    expected_dict.update(config_dict)
+
+    assert config.to_dict() == expected_dict
+
+    config = Config() # issue, still contains default variable
+    assert config.to_dict() == config_dict
